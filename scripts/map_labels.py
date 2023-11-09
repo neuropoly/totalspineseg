01a34c75365f93e8f1b509f31183a1310787c421
yw7 <36595323+yw7@users.noreply.github.com>
2023-11-09 17:11:00 +0200

    Generalize and multithread map_labels script

--- scripts/map_labels.py
+++ scripts/map_labels.py
@@ -4,6 +4,9 @@ from pathlib import Path
 import numpy as np
 import nibabel as nib
 
+import multiprocessing as mp
+from tqdm.contrib.concurrent import process_map
+from functools import partial
 
 def main():
     
@@ -11,30 +14,17 @@ def main():
     parser = argparse.ArgumentParser(
         description=textwrap.dedent(f'''
             Map segmentation labels to other labels using json mapping file.
-            
-            The data is assumed to follow the BIDS structure (or similar):
-
-            labels
-            ├── sub-1
-            │   └── anat
-            │       ├── sub-1_T1w_seg.nii.gz
-            │       └── sub-1_T2w_seg.nii.gz
-            └── sub-2
-                └── anat
-                    ├── sub-2_T1w_seg.nii.gz
-                    └── sub-2_T2w_seg.nii.gz
-            
-
         '''),
         epilog=textwrap.dedent('''
-            Example:
-            map_labels -d derivatives/labels_orig -o derivatives/labels
+            Examples:
+            map_labels -i labels -o labels_maped -m map.json
+            map_labels -i labels -o labels_maped  -m map.json -d sub- -s anat -w 32
         '''),
         formatter_class=argparse.RawTextHelpFormatter
     )
 
     parser.add_argument(
-        '--seg-dir', '-d', type=DirPath(), required=True,
+        '--seg-dir', '-i', type=DirPath(), required=True,
         help='Folder containing input segmentations for each subject.'
     )
     parser.add_argument(
@@ -46,20 +36,36 @@ def main():
         help='JSON file mapping each mask to a unique number, e.g. {"1": 2, "2": 15}'
     )
     parser.add_argument(
-        '--subject-prefix', type=str, default='sub-',
-        help='Subject prefix in filenames, defaults to "sub-" (BIDS standard).'
+        '--subject-dir', '-d', type=str, default=None, nargs='?', const='',
+        help=textwrap.dedent('''
+            Is every subject has its oen direcrory.
+            If this argument will be provided without value it will look for any directory in the segmentation directory.
+            If value also provided it will be used as a prefix to subject directory, defaults to False (no subjet directory).
+        '''),
     )
     parser.add_argument(
-        '--subject-subdir', type=str, default='anat', 
-        help='Subfolder inside subject folder containing masks, defaults to "anat" (BIDS standard).'
+        '--subject-subdir', '-s', type=str, default='', 
+        help='Subfolder inside subject folder containing masks, defaults to no subfolder.'
     )
     parser.add_argument(
-        '--seg-suffix', type=str, default='_seg',
-        help='Suffix for output segmentation, defaults to "_seg".'
+        '--seg-suffix', type=str, default='',
+        help='Suffix for output segmentation, defaults to "".'
     )
     parser.add_argument(
-        '--output-seg-suffix', type=str, default='_seg',
-        help='Suffix for output segmentation, defaults to "_seg".'
+        '--output-seg-suffix', type=str, default='',
+        help='Suffix for output segmentation, defaults to "".'
+    )
+    parser.add_argument(
+        '--default-input', action="store_true", default=False,
+        help='Init output from input, defaults to false (init all to 0).'
+    )
+    parser.add_argument(
+        '--add-output', action="store_true", default=False,
+        help='Add new mapped labels to output if the output file exist, defaults to false (Overrite the output file).'
+    )
+    parser.add_argument(
+        '--max-workers', '-w', type=int, default=min(32, mp.cpu_count() + 4),
+        help='Max worker to run in parallel proccess, defaults to min(32, mp.cpu_count() + 4).'
     )
     parser.add_argument(
         '--verbose', '-v', type=int, default=1, choices=[0, 1],
@@ -75,10 +81,13 @@ def main():
     seg_path = args.seg_dir
     output_path = args.output_dir
     map_file = args.map
-    subject_prefix = args.subject_prefix
+    subject_dir = args.subject_dir
     subject_subdir = args.subject_subdir
     seg_suffix = args.seg_suffix
     output_seg_suffix = args.output_seg_suffix
+    default_input = args.default_input
+    add_output = args.add_output
+    max_workers = args.max_workers
     verbose = args.verbose
 
     if verbose:
@@ -87,10 +96,13 @@ def main():
             seg_dir = "{seg_path}"
             output_dir = "{output_path}"
             map = "{map_file.name}"
-            subject_prefix = "{subject_prefix}"
+            subject_dir = "{subject_dir}"
             subject_subdir = "{subject_subdir}"
             seg_suffix = "{seg_suffix}"
             output_seg_suffix = "{output_seg_suffix}"
+            default_input = "{default_input}"
+            add_output = "{add_output}"
+            max_workers = "{max_workers}"
             verbose = {verbose}
         '''))
 
@@ -98,63 +110,62 @@ def main():
     map_dict = json.load(map_file)
     map_file.close()
 
-    # Process NIfTI segmentation files
-    subject_dirs = list(seg_path.glob(f'{subject_prefix}*'))
+    glob_pattern = ""
+    if subject_dir is not None:
+        glob_pattern += f"{subject_dir}*/"
+    if len(subject_subdir) > 0:
+        glob_pattern += f"{subject_subdir}/"
+    glob_pattern += f'*{seg_suffix}.nii.gz'
+
+    # Process the NIfTI image and segmentation files
+    segs_path_list = list(seg_path.glob(glob_pattern))
+
+    # Create a partially-applied function with the extra arguments
+    partial_map_seg = partial(map_seg, map_dict=map_dict, output_path=output_path, seg_suffix=seg_suffix, output_seg_suffix=output_seg_suffix, add_output=add_output, default_input=default_input)
+
+    with mp.Pool() as pool:
+        process_map(partial_map_seg, segs_path_list, max_workers=max_workers)
     
-    for i, subject_dir in enumerate(subject_dirs):
-        
-        subject = subject_dir.name
-        
-        # Check that subject subdir exists
-        if not (subject_dir / subject_subdir).is_dir():
-            if verbose: 
-                print(f'Warning: "{subject_subdir}" not found for {subject}')
-            continue
-            
-        if verbose:
-            print(f'Processing {subject} ({i+1}/{len(subject_dirs)})')
-            
-        # Process each segmentation file
-        for seg_path in (subject_dir / subject_subdir).glob(f'*{seg_suffix}.nii.gz'):
-            
-            output_seg_path = output_path / subject / subject_subdir / seg_path.name.replace(f'{seg_suffix}.nii.gz', f'{output_seg_suffix}.nii.gz')
 
-            # Load segmentation
-            seg = nib.load(seg_path)
-            seg_data = seg.get_fdata()
+def map_seg(seg_path, map_dict, output_path, seg_suffix, output_seg_suffix, add_output, default_input):
+    
+    output_seg_path = output_path / seg_path.name.replace(f'{seg_suffix}.nii.gz', f'{output_seg_suffix}.nii.gz')
 
-            # Convert data to uint8 to avoid issues with segmentation IDs
-            seg_data = seg_data.astype(np.uint8) 
+    # Load segmentation
+    seg = nib.load(seg_path)
+    seg_data = seg.get_fdata()
 
-            # Apply label mapping
-            mapped_seg_data = np.zeros_like(seg_data)
-            
-            # Apply label mapping for all labels that are not mapped to 0
-            for orig, new in map_dict.items():
-                if new != 0:
-                    mapped_seg_data[seg_data==int(orig)] = int(new)
+    # Convert data to uint8 to avoid issues with segmentation IDs
+    seg_data = seg_data.astype(np.uint8)
 
-            # Restores the original segmentation values for any voxels that were not mapped, to avoid losing data.
-            mapped_seg_data = np.where(mapped_seg_data == 0, seg_data, mapped_seg_data)
-                    
-            # Handle mapping labels to 0  
-            for orig, new in map_dict.items():
-                if new == 0:
-                    mapped_seg_data[seg_data==int(orig)] = int(new)
+    # Apply label mapping
+    mapped_seg_data = np.copy(seg_data) if default_input else np.zeros_like(seg_data)
+    
+    # Apply label mapping for all labels that are not mapped to 0
+    for orig, new in map_dict.items():
+        mapped_seg_data[seg_data==int(orig)] = int(new)
 
-            # Create result segmentation 
-            mapped_seg = nib.Nifti1Image(mapped_seg_data, seg.affine, seg.header)
-            mapped_seg.set_data_dtype(np.uint8)
+    # Add new mapped labels to output
+    if add_output and output_seg_path.is_file():
+        # Load segmentation
+        output_seg = nib.load(output_seg_path)
+        output_seg_data = output_seg.get_fdata()
 
-            # Make sure output directory exists
-            output_seg_path.parent.mkdir(parents=True, exist_ok=True)
-            
-            # Save mapped segmentation
-            nib.save(mapped_seg, output_seg_path)
-            
-            if verbose:
-                print(f"Saved mapped {seg_path} to {output_seg_path}")
-                
+        # Convert data to uint8 to avoid issues with segmentation IDs
+        output_seg_data = output_seg_data.astype(np.uint8)
+
+        # Update output from existing output file
+        mapped_seg_data[output_seg_data != 0] = output_seg_data[output_seg_data != 0]
+
+    # Create result segmentation 
+    mapped_seg = nib.Nifti1Image(mapped_seg_data, seg.affine, seg.header)
+    mapped_seg.set_data_dtype(np.uint8)
+
+    # Make sure output directory exists
+    output_seg_path.parent.mkdir(parents=True, exist_ok=True)
+    
+    # Save mapped segmentation
+    nib.save(mapped_seg, output_seg_path)
 
 class DirPath:
     """
