6f8652ef69a24ff1e1582636d63d33ffb1be8d8d
yw7 <36595323+yw7@users.noreply.github.com>
2024-01-31 02:03:25 +0200

    Enhance training script output verbosity

--- run/train_nnunet.sh
+++ run/train_nnunet.sh
@@ -35,50 +35,55 @@ export nnUNet_preprocessed=data/nnUNet/nnUNet_preprocessed
 export nnUNet_results=data/nnUNet/nnUNet_results
 
 for d in 206 210; do
-    # Make nnUNet raw folders
+    echo "Make nnUNet raw folders ($d)"
     mkdir -p data/nnUNet/nnUNet_raw/Dataset${d}_TotalSegMRI/imagesTr
     mkdir -p data/nnUNet/nnUNet_raw/Dataset${d}_TotalSegMRI/labelsTr
     
-    # Copy dataset file
+    echo "Copy dataset file"
     cp totalsegmentator-mri/src/totalsegmri/resources/datasets/dataset_${d}.json data/nnUNet/nnUNet_raw/Dataset${d}_TotalSegMRI/dataset.json
 done
 
 # Convert from BIDS to nnUNet dataset, loop over each dataset
 for ds in spider data-single-subject data-multi-subject whole-spine; do
+    echo "Working on $ds"
+
     # Set segmentation file name based on dataset, 'seg' for 'spider', 'PAM50_seg' otherwise.
     [ "$ds" = "spider" ] && seg="seg" || seg="PAM50_seg"
 
-    # Copy image and label files to the nnUNet dataset structure
+    echo "Copy images and labels into the nnUNet dataset folder"
     cp data/bids/$ds/sub-*/anat/sub-*.nii.gz data/nnUNet/nnUNet_raw/Dataset206_TotalSegMRI/imagesTr
     cp data/bids/$ds/derivatives/labels/sub-*/anat/sub-*_$seg.nii.gz data/nnUNet/nnUNet_raw/Dataset206_TotalSegMRI/labelsTr
     
     # Format dataset name by removing 'data-' and '-subject' or '-spine'
     dsn=${ds/data-/}; dsn=${dsn/-subject/}; dsn=${dsn/-spine/}
 
-    # Rename nnUNet dataset: replace 'sub-' with dataset name, remove segmentation identifier.
+    echo "Replace 'sub-' with dataset name"
     for f in data/nnUNet/nnUNet_raw/Dataset206_TotalSegMRI/*Tr/sub-*.nii.gz; do mv $f ${f/sub-/${dsn}_}; done
+
+    echo "Remove _$seg from files name"
     for f in data/nnUNet/nnUNet_raw/Dataset206_TotalSegMRI/labelsTr/*_$seg.nii.gz; do mv $f ${f/_$seg/}; done
 done
-# Append '_0000' to the images
+
+echo "Append '_0000' to the images names"
 for f in data/nnUNet/nnUNet_raw/Dataset206_TotalSegMRI/imagesTr/*.nii.gz; do mv $f ${f/.nii.gz/_0000.nii.gz}; done
 
-# Fix csf label to include all non cord spinal canal, this will put the spinal canal label in all the voxels (labeled as a backgroupn) between the spinal canal and the spinal cord.
+echo "Fix csf label to include all non cord spinal canal, this will put the spinal canal label in all the voxels (labeled as a backgroupn) between the spinal canal and the spinal cord."
 python totalsegmentator-mri/src/totalsegmri/utils/fix_csf_label.py -i data/nnUNet/nnUNet_raw/Dataset206_TotalSegMRI/labelsTr -o data/nnUNet/nnUNet_raw/Dataset206_TotalSegMRI/labelsTr
 
-# Crop images and segmentations in the most anteior voxel of the lowest vertebrae in the image or at the lowest voxel of T12-L1 IVD (for SPIDER dataset).
+echo "Crop images and segmentations in the most anteior voxel of the lowest vertebrae in the image or at the lowest voxel of T12-L1 IVD (for SPIDER dataset)."
 for dsn in single multi whole; do
     python totalsegmentator-mri/src/totalsegmri/utils/generate_croped_images.py -p ${dsn}_ -i data/nnUNet/nnUNet_raw/Dataset206_TotalSegMRI/imagesTr -s data/nnUNet/nnUNet_raw/Dataset206_TotalSegMRI/labelsTr -o data/nnUNet/nnUNet_raw/Dataset206_TotalSegMRI/imagesTr -g data/nnUNet/nnUNet_raw/Dataset206_TotalSegMRI/labelsTr
 done
 python totalsegmentator-mri/src/totalsegmri/utils/generate_croped_images.py -p spider_ -i data/nnUNet/nnUNet_raw/Dataset206_TotalSegMRI/imagesTr -s data/nnUNet/nnUNet_raw/Dataset206_TotalSegMRI/labelsTr -o data/nnUNet/nnUNet_raw/Dataset206_TotalSegMRI/imagesTr -g data/nnUNet/nnUNet_raw/Dataset206_TotalSegMRI/labelsTr --from-bottom
 
-# Copy from 206 to 210 dataset
+echo "Copy from 206 to 210 dataset"
 for f in data/nnUNet/nnUNet_raw/Dataset206_TotalSegMRI/*Tr/*.nii.gz; do cp $f ${f/206/210}; done
 
-# Map labels to 210 2'nd channel
-python totalsegmentator-mri/src/totalsegmri/utils/map_labels.py -m totalsegmentator-mri/src/totalsegmri/resources/labels_maps/nnunet_210_0001.json -i data/nnUNet/nnUNet_raw/Dataset210_TotalSegMRI/labelsTr -o data/nnUNet/nnUNet_raw/Dataset${d}_TotalSegMRI/imagesTr --output-seg-suffix _0001
+echo "Map labels to 210 2'nd channel"
+python totalsegmentator-mri/src/totalsegmri/utils/map_labels.py -m totalsegmentator-mri/src/totalsegmri/resources/labels_maps/nnunet_210_0001.json -i data/nnUNet/nnUNet_raw/Dataset210_TotalSegMRI/labelsTr -o data/nnUNet/nnUNet_raw/Dataset210_TotalSegMRI/imagesTr --output-seg-suffix _0001
 
 for d in 206 210; do
-    # Map original labels to the dataset specific labels
+    echo "Map original labels to the dataset specific labels"
     python totalsegmentator-mri/src/totalsegmri/utils/map_labels.py -m totalsegmentator-mri/src/totalsegmri/resources/labels_maps/nnunet_${d}.json -i data/nnUNet/nnUNet_raw/Dataset${d}_TotalSegMRI/labelsTr -o data/nnUNet/nnUNet_raw/Dataset${d}_TotalSegMRI/labelsTr
     
     # Preprocess
