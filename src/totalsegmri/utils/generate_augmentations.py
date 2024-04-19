c6b602c1a18f5b8b051f9419a4c5512a4dc9372e
yw7 <36595323+yw7@users.noreply.github.com>
2024-04-20 02:02:38 +0300

    Enhance MRI augmentation flexibility
    Introduced segmentation class specification for targeted MRI image augmentations, streamlining class-based variation. The modification allows specifying a range of label classes for per-class augmentation defaults, improving customization for image transformations. Adjusted augmentation probabilities and conditions to refine the distribution and type of augmentations applied, focusing on contrast, artifacts, and spatial changes, to enhance the realism and variety of synthetic MRI data.
    
    Issue references: #24

--- src/totalsegmri/utils/generate_augmentations.py
+++ src/totalsegmri/utils/generate_augmentations.py
@@ -85,6 +85,10 @@ def main():
         help='Use Random Labels To Image augmentation, defaults to false.'
     )
     parser.add_argument(
+        '--seg-classes', type=parse_class, nargs='+', default=[parse_class(_) for _ in ['202-224', '18-41,92', '200', '201', ]],
+        help='Define classes of labels for per class augmentation. Default 202-224 18-41,92 200 201 (IVDs, vertebrae, disc, csf).'
+    )
+    parser.add_argument(
         '--max-workers', '-w', type=int, default=min(32, mp.cpu_count() + 4),
         help='Max worker to run in parallel proccess, defaults to min(32, mp.cpu_count() + 4).'
     )
@@ -113,6 +117,7 @@ def main():
     output_seg_suffix = args.output_seg_suffix
     augmentations_per_image = args.augmentations_per_image
     labels2image = args.labels2image
+    seg_classes = args.seg_classes
     max_workers = args.max_workers
     verbose = args.verbose
     
@@ -133,6 +138,7 @@ def main():
             output_seg_suffix = "{output_seg_suffix}"
             augmentations_per_image = "{augmentations_per_image}"
             labels2image = "{labels2image}"
+            seg_classes = "{seg_classes}"
             max_workers = "{max_workers}"
             verbose = "{verbose}"
         '''))
@@ -160,6 +166,7 @@ def main():
         output_seg_suffix=output_seg_suffix,
         augmentations_per_image=augmentations_per_image,
         labels2image=labels2image,
+        seg_classes=seg_classes,
     )
 
     with mp.Pool() as pool:
@@ -178,6 +185,7 @@ def generate_augmentations(
         output_seg_suffix,
         augmentations_per_image,
         labels2image,
+        seg_classes,
     ):
     
     seg_path = segs_path / image_path.relative_to(images_path).parent /  image_path.name.replace(f'{image_suffix}.nii.gz', f'{seg_suffix}.nii.gz')
@@ -196,42 +204,54 @@ def generate_augmentations(
         output_seg_path = output_segs_path / image_path.relative_to(images_path).parent / seg_path.name.replace(f'{seg_suffix}.nii.gz', f'_a{i}{output_seg_suffix}.nii.gz')
 
         augs = []
-        
-        # Randomly add aug_labels2image, aug_flip, aug_inverse with respective probabilities
-        if labels2image and np.random.rand() < 0.1:
-            augs.append(aug_labels2image)
-        if np.random.rand() < 0.3:
-            augs.append(aug_flip)
+
+        # Contrast augmentation
+        if np.random.rand() < 0.8:
+                _aug_redistribute_seg = partial(aug_redistribute_seg, classes=seg_classes)
+                augs.append(np.random.choice([
+                    _aug_redistribute_seg,
+                    aug_gamma,
+                    np.random.choice([
+                        aug_log,
+                        aug_sqrt,
+                        aug_exp,
+                        aug_sin,
+                        aug_sig,
+                    ]),
+                ], p=[0.4, 0.3, 0.3]))
+
+        # Image form segmentation augmentation
+        elif labels2image and np.random.rand() < 0.5:
+            _aug_labels2image = partial(aug_labels2image, classes=seg_classes)
+            augs.append(_aug_labels2image)
+
+        # Inverse color augmentation
         if np.random.rand() < 0.3:
             augs.append(aug_inverse)
-        
-        augs.extend(np.random.choice([
-                np.random.choice([
-                    aug_bspline,
-                    aug_aff,
-                    aug_elastic,
-                ]),
-                np.random.choice([
-                    aug_log,
-                    aug_sqrt,
-                    aug_exp,
-                    aug_sin,
-                    aug_sig,
-                    aug_gamma,
-                ]),
-                np.random.choice([
-                    aug_motion,
-                    aug_ghosting,
-                    aug_spike,
-                    aug_bias_field,
-                    aug_blur,
-                    aug_noise,
-                ]),
-            ],
-            np.random.choice(range(1,4)),
-            False
-        ))
 
+        # Artifacts augmentation
+        if np.random.rand() < 0.7:
+            augs.append(np.random.choice([
+                aug_motion,
+                aug_ghosting,
+                aug_spike,
+                aug_bias_field,
+                aug_blur,
+                aug_noise,
+            ]))
+
+        # Spatial augmentation
+        if np.random.rand() < 0.3:
+            augs.append(aug_flip)
+
+        if np.random.rand() < 0.7:
+            augs.append(np.random.choice([
+                aug_bspline,
+                aug_aff,
+                aug_elastic,
+            ]))
+
+        # Anisotropy augmentation
         downsampling = max(1, 7 / max(image.header.get_zooms()))
         _aug_anisotropy = partial(aug_anisotropy, downsampling=(min(1.5, downsampling), downsampling))
         if np.random.rand() < 0.7:
@@ -256,14 +276,56 @@ def generate_augmentations(
         nib.save(output_image, output_image_path)
         nib.save(output_seg, output_seg_path)
 
+def aug_redistribute_seg(img, seg, factor=(-0.2, 0.7), classes=None):
+
+    # Ensure factor is a list
+    factor=np.array([factor]).flat
+
+    if classes:
+        seg = combine_classes(seg, classes)
+
+    # Compute original mean, std and min/max values
+    original_mean, original_std = np.mean(img), np.std(img)
+    img_min, img_max = np.min(img), np.max(img)
+
+    # Normlize
+    img = (img - original_mean) / original_std
+    img = (img - img.min()) / (img.max() - img.min())
+
+    # Get the unique label values (excluding 0)
+    labels = [_ for _ in np.unique(seg) if _ != 0]
+    
+    to_add = np.zeros_like(img)
+    # Loop over each label value
+    for l in labels:
+        # Get the mask for the current label
+        l_mask = (seg == l)
+        l_mean = np.mean(img[l_mask])
+        l_std = np.std(img[l_mask])
+
+        # Change std
+        l_std *= np.random.uniform(0.9, 1.1)
+
+        # Calculate the normal distribution
+        exponent = -((img - l_mean) ** 2) / (2 * l_std ** 2)
+        coeff = 1 / (l_std * np.sqrt(2 * np.pi))
+        to_add += coeff * np.exp(exponent) * np.random.uniform(-1, 1)
+
+    img += (to_add - to_add.min()) / ((to_add.max() - to_add.min()) / np.random.uniform(factor[0], factor[-1]))
+
+    # Return to original range
+    img = np.interp(img, (img_min, img_max), (img_min, img_max))
+
+    return img, seg
+
 def aug_transform(img, seg, transform):
     # Compute original mean, std and min/max values
     original_mean, original_std = np.mean(img), np.std(img)
     img_min, img_max = np.min(img), np.max(img)
 
     # Generate random mean and std
-    random_mean = np.random.uniform(img_min, img_max)
-    random_std = np.random.uniform(0.5 * original_std, 2 * original_std)
+    random_mean = np.random.rand() * (img_max - img_min) + img_min
+    random_std = (np.random.rand() * 1.5 + 0.5) * original_std
 
     # Normlize
     img = (img - original_mean) / original_std
@@ -386,7 +448,9 @@ def aug_swap(img, seg):
     ))
     return subject.image.data.squeeze().numpy(), subject.seg.data.squeeze().numpy().astype(np.uint8)
 
-def aug_labels2image(img, seg):
+def aug_labels2image(img, seg, classes=None):
+    if classes:
+        seg = combine_classes(seg, classes)
     subject = tio.RandomLabelsToImage(label_key="seg", image_key="image")(tio.Subject(
         seg=tio.LabelMap(tensor=np.expand_dims(seg, axis=0))
     ))
@@ -399,6 +463,16 @@ def aug_gamma(img, seg):
     ))
     return subject.image.data.squeeze().numpy(), subject.seg.data.squeeze().numpy().astype(np.uint8)
 
+def parse_class(c):
+    c = [_.split('-') for _ in c.split(',')]
+    c = tuple(__ for _ in c for __ in list(range(int(_[0]), int(_[-1]) + 1)))
+    return c
+
+def combine_classes(seg, classes):
+    seg = np.zeros_like(_seg:=seg)
+    for i, c in enumerate(classes):
+        seg[np.isin(_seg, c)] = i + 1
+    return seg
 
 if __name__ == '__main__':
     main()
