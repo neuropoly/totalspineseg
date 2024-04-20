55a0f143cb40360f814c52c1bd1ca62dc1669a08
yw7 <36595323+yw7@users.noreply.github.com>
2024-04-20 15:01:11 +0300

    Refactor MRI segmentation JPEG conversion
    Replaced nibabel and nilearn dependencies with torchio for MRI image and segmentation processing to streamline the codebase. Added flexibility by allowing segmentation directory to be optional and broadening input image file type support. Simplified random color generation for segment overlays ensuring consistent label colors and adjusted image normalization and combination logic. This enhancement improves overall code efficiency and performance, as well as extendability for future features.

--- src/totalsegmri/utils/generate_seg_jpg.py
+++ src/totalsegmri/utils/generate_seg_jpg.py
@@ -1,16 +1,16 @@
 import sys, argparse, textwrap
 import numpy as np
-import nibabel as nib
-from nibabel import freesurfer
 from PIL import Image
-from nilearn import image as nl_image
 import multiprocessing as mp
 from functools import partial
 from pathlib import Path
 from tqdm.contrib.concurrent import process_map
+import torchio as tio
 import warnings
 
 warnings.filterwarnings("ignore")
+
+EXT = ['nii.gz', 'mgz']
 
 def main():
 
@@ -34,7 +34,7 @@ def main():
         help='The folder where input NIfTI images files are located (required).'
     )
     parser.add_argument(
-        '--segs-dir', '-s', type=Path, required=True,
+        '--segs-dir', '-s', type=Path, default=None,
         help='The folder where input NIfTI segmentation files are located (required).'
     )
     parser.add_argument(
@@ -131,10 +131,10 @@ def main():
         glob_pattern += f"{subject_dir}*/"
     if len(subject_subdir) > 0:
         glob_pattern += f"{subject_subdir}/"
-    glob_pattern += f'{prefix}*{image_suffix}.nii.gz'
+    glob_pattern += f'{prefix}*{image_suffix}'
 
     # Process the NIfTI image and segmentation files
-    images_path_list = list(images_path.glob(glob_pattern))
+    images_path_list = [_ for __ in [list(images_path.glob(f'{glob_pattern}.{e}')) for e in EXT] for _ in __]
 
     # Create a partially-applied function with the extra arguments
     partial_generate_seg_jpg = partial(
@@ -154,78 +154,65 @@ def main():
     
 
 def generate_seg_jpg(image_path, segs_path, images_path, output_path, seg_suffix, image_suffix, orient, sliceloc, override):
-    
-    seg_path = segs_path / image_path.relative_to(images_path).parent /  image_path.name.replace(f'{image_suffix}.nii.gz', f'{seg_suffix}.nii.gz')
-    if not seg_path.is_file():
-        seg_path = seg_path.parent / seg_path.name.replace('.nii.gz', '.mgz')
-    output_jpg_path = output_path / image_path.relative_to(images_path).parent / image_path.name.replace(f'{image_suffix}.nii.gz', f'_{orient}_{sliceloc}.jpg')
-    
-    if not image_path.is_file() or not seg_path.is_file():
-        return
-    
+
+    image_ext = [e for e in EXT if image_path.name.endswith(e)][0]
+    output_jpg_path = output_path / image_path.relative_to(images_path).parent / image_path.name.replace(f'{image_suffix}.{image_ext}', f'_{orient}_{sliceloc}.jpg')
+
     # Check if the output file exists and if the override flag is set to 0
     if output_jpg_path.exists() and not override:
         return
 
-    # Set the seed to ensure consistent colors for each label
-    np.random.seed(42)
+    img = tio.ScalarImage(image_path)
+    img = tio.ToCanonical()(img)
+    img = tio.Resample((1, 1, 1))(img)
 
-    # Load the NIfTI image file
-    img = nib.load(image_path)
-
-    # Load the segmentation file (NIfTI or MGZ)
-    if seg_path.suffix == '.mgz':
-        seg = freesurfer.load(seg_path)
-        # Convert the MGHImage to a NIfTI image
-        seg = nib.Nifti1Image(seg.get_fdata(), seg.affine)
-    else:
-        seg = nib.load(seg_path)
-
-    # Reorient the input image to RAS
-    img = nib.as_closest_canonical(img)
-    seg = nib.as_closest_canonical(seg)
-
-    # Resample the segmentation to 1X1 mm
-    seg = nl_image.resample_img(seg, np.eye(3), interpolation='nearest')
-
-    # Resample the image to the same space as the segmentation
-    img = nl_image.resample_to_img(img, seg)
-
-    # Convert the image and segmentation data into numpy arrays
-    img_data = img.get_fdata()
-    seg_data = seg.get_fdata()
+    img_data = img.data.squeeze().numpy()
 
     # Find the specified slice
     axis={'sag': 0, 'cor': 1, 'ax': 2}[orient]
     slice_index = int(sliceloc * img_data.shape[axis])
     slice_img = img_data.take(slice_index, axis=axis)
-    slice_seg = seg_data.take(slice_index, axis=axis)
-
-    # Generate consistent random colors for each label
-    unique_labels = np.unique(slice_seg).astype(int)
-    colors = {}
-    for label in unique_labels:
-        np.random.seed(label)
-        colors[label] = np.random.randint(0, 255, 3)
-
-    # Create a blank color image with the same dimensions as the input image
-    color_img = np.zeros((*slice_img.shape, 3), dtype=np.uint8)
-
-    # Apply the segmentation mask to the image and assign colors
-    for label, color in colors.items():
-        if label != 0:  # Ignore the background (label 0)
-            mask = slice_seg == label
-            color_img[mask] = color
 
     # Normalize the slice to the range 0-255
-    normalized_slice = (255 * (slice_img - np.min(slice_img)) / (np.max(slice_img) - np.min(slice_img))).astype(np.uint8)
+    slice_img = (255 * (slice_img - np.min(slice_img)) / (np.max(slice_img) - np.min(slice_img))).astype(np.uint8)
 
-    # Combine the original grayscale image with the colored segmentation mask
-    grayscale_img = np.repeat(normalized_slice[:, :, np.newaxis], 3, axis=2).astype(np.uint8)
-    combined_img = np.where(color_img > 0, color_img, grayscale_img)
+    # Repeat the grayscale slice 3 times to create a color image
+    slice_img = np.repeat(slice_img[:, :, np.newaxis], 3, axis=2).astype(np.uint8)
+
+    # Create a blank color image with the same dimensions as the input image
+    color_img = np.zeros_like(slice_img, dtype=np.uint8)
+
+    if segs_path:
+
+        seg_path = segs_path / image_path.relative_to(images_path).parent / image_path.name.replace(f'{image_suffix}.{image_ext}', f'{seg_suffix}')
+        seg_path = [seg_path.parent / f'{seg_path.name}.{e}' for e in EXT if (seg_path.parent / f'{seg_path.name}.{e}').is_file()][0]
+        
+        if seg_path.is_file():
+            seg = tio.LabelMap(seg_path)
+            seg = tio.ToCanonical()(seg)
+            seg = tio.Resample(img)(seg)
+
+            seg_data = seg.data.squeeze().numpy()
+
+            slice_seg = seg_data.take(slice_index, axis=axis)
+
+            # Generate consistent random colors for each label
+            unique_labels = np.unique(slice_seg).astype(int)
+            colors = {}
+            for label in unique_labels:
+                rs = np.random.RandomState(np.random.MT19937(np.random.SeedSequence(label)))
+                colors[label] = rs.randint(0, 255, 3)
+
+            # Apply the segmentation mask to the image and assign colors
+            for label, color in colors.items():
+                if label != 0:  # Ignore the background (label 0)
+                    mask = slice_seg == label
+                    color_img[mask] = color
+
+    color_img = np.where(color_img > 0, color_img, slice_img)
 
     # Rotate the image 90 degrees counter-clockwise and flip it vertically
-    output_img = np.flipud(combined_img)
+    output_img = np.flipud(color_img)
     output_img = np.rot90(output_img, k=1)
 
     # Create an Image object from the output image
