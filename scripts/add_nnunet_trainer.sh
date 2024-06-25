d520c97e44bf801fa92026741f941e4fde170463
yw7 <36595323+yw7@users.noreply.github.com>
2024-06-25 23:06:05 +0300

    feat: Add nnUNetTrainer_DA5SegOrd0_NoMirroring_16000epochs
    Create a specialized trainer class with 16,000 epochs and data augmentation that omits mirroring. Refactor inference and training script configurations to use the new trainer. Improve script resilience by checking for the existence of the target file before writing the new trainer definition. Enhance inference script output with clear naming for segmented JPG previews at step1 and 2 steps.

--- scripts/add_nnunet_trainer.sh
+++ scripts/add_nnunet_trainer.sh
@@ -19,3 +19,27 @@ class nnUNetTrainer_DASegOrd0_NoMirroring_16000epochs(nnUNetTrainer_DASegOrd0_No
         self.num_epochs = 16000
 EOL
 fi
+
+# Define the target file
+TARGET_FILE="$NNUNETV2_PATH/training/nnUNetTrainer/nnUNetTrainer_DA5SegOrd0_NoMirroring_16000epochs.py"
+
+# Ensure the target file exists and create it if it is not
+if [ ! -f $TARGET_FILE ]; then
+  # Add the class definition to the target file
+  cat <<EOL > "$TARGET_FILE"
+import torch
+from nnunetv2.training.nnUNetTrainer.variants.data_augmentation.nnUNetTrainerDA5 import nnUNetTrainerDA5Segord0
+class nnUNetTrainer_DA5SegOrd0_NoMirroring_16000epochs(nnUNetTrainerDA5Segord0):
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
+                 device: torch.device = torch.device('cuda')):
+        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        self.num_epochs = 16000
+
+    def configure_rotation_dummyDA_mirroring_and_inital_patch_size(self):
+        rotation_for_DA, do_dummy_2d_data_aug, initial_patch_size, mirror_axes = \
+            super().configure_rotation_dummyDA_mirroring_and_inital_patch_size()
+        mirror_axes = None
+        self.inference_allowed_mirroring_axes = None
+        return rotation_for_DA, do_dummy_2d_data_aug, initial_patch_size, mirror_axes
+EOL
+fi
