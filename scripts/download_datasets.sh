50eb066acadabc2553c1aef19090382c14980b0a
Nathan Molinier <nathan.molinier@gmail.com>
2025-06-23 16:29:35 -0400

    get data for testing

--- scripts/download_datasets.sh
+++ scripts/download_datasets.sh
@@ -85,5 +85,12 @@ for key in "${keys[@]}"; do
     done
 done
 
+for key in "${keys[@]}"; do
+    for path in $(jq -r ".TESTING | .[].$key" "$data_json"); do
+        IFS='/' read -r rep_path rel_path <<< "$path"
+        git -C "$rep_path" annex get "$rel_path"
+    done
+done
+
 # Return to the original working directory
 cd "$CURR_DIR"
