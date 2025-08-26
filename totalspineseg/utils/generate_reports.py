95c1f1055e228e1980d5a23dce1f0f2a3060ce09
Nathan Molinier <nathan.molinier@gmail.com>
2025-08-26 15:59:54 -0400

    fix vertebrae label displayed

--- totalspineseg/utils/generate_reports.py
+++ totalspineseg/utils/generate_reports.py
@@ -453,16 +453,16 @@ def create_global_figures(subject_data, all_values_df, discs_gap, mean_dict, img
                 ax.plot(x_subject, y_subject, color='red', linewidth=2)
                 
                 # Add vertebrae labels
-                bot_pos = 0
+                top_pos = 0
                 for disc, gap in discs_gap.items():
-                    bot_vert = rev_mapping[int(float(disc.split('-')[0]))].split('-')[1]
-                    ax.axvline(x=bot_pos, color='gray', linestyle='--', alpha=0.5)
-                    ax.text(bot_pos + gap//2, ax.get_ylim()[1], bot_vert, verticalalignment='bottom', horizontalalignment='center', fontsize=12, color='black', alpha=0.7)
-                    bot_pos += gap
+                    top_vert = rev_mapping[int(float(disc.split('-')[0]))].split('-')[0]
+                    ax.axvline(x=top_pos, color='gray', linestyle='--', alpha=0.5)
+                    ax.text(top_pos + gap//2, ax.get_ylim()[1], top_vert, verticalalignment='bottom', horizontalalignment='center', fontsize=12, color='black', alpha=0.7)
+                    top_pos += gap
                 # Add last vertebra
-                last_vert = rev_mapping[int(float(disc.split('-')[1]))].split('-')[0]
-                ax.axvline(x=bot_pos, color='gray', linestyle='--', alpha=0.5)
-                ax.text(bot_pos + gap//2, ax.get_ylim()[1], last_vert, verticalalignment='bottom', horizontalalignment='center', fontsize=12, color='black', alpha=0.7)
+                last_vert = rev_mapping[int(float(disc.split('-')[0]))].split('-')[1]
+                ax.axvline(x=top_pos, color='gray', linestyle='--', alpha=0.5)
+                ax.text(top_pos + gap//2, ax.get_ylim()[1], last_vert, verticalalignment='bottom', horizontalalignment='center', fontsize=12, color='black', alpha=0.7)
 
                 fig.tight_layout()
                 idx += 1
