6e2cc9d33503e3c52a0cb8e126a5caddd2215d73
Nathan Molinier <nathan.molinier@gmail.com>
2025-09-10 09:58:11 -0400

    check if vertebrae exist for foramen

--- totalspineseg/utils/generate_reports.py
+++ totalspineseg/utils/generate_reports.py
@@ -319,6 +319,11 @@ def compute_foramens_metrics(data_dict):
     # Compute Foramen metrics
     for struc_name in data_dict['foramens'].keys():
         top_vertebra = struc_name.replace('foramens_','').split('-')[0]
+        if not top_vertebra in data_dict['vertebrae']:
+            data_dict['foramens'][struc_name]['right_surface'] = -1
+            data_dict['foramens'][struc_name]['left_surface'] = -1
+            data_dict['foramens'][struc_name]['asymmetry R/L'] = -1
+            continue
         # Normalize foramen surfaces with top vertebra AP thickness
         for surface in ['right_surface', 'left_surface']:
             data_dict['foramens'][struc_name][surface] = data_dict['foramens'][struc_name][surface] / (data_dict['vertebrae'][top_vertebra]['AP_thickness']*data_dict['vertebrae'][top_vertebra]['median_thickness'])
